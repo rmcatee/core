8f716140521ade15118670e2f3c01be2131b28ad
Jörn Friedrich Dreyer <jfd@butonic.de>
2013-05-08 16:29:15 +0200

    cleanup codestyle

--- lib/files/cache/cache.php
+++ lib/files/cache/cache.php
@@ -145,7 +145,7 @@ class Cache {
 				 FROM `*PREFIX*filecache` WHERE `parent` = ? ORDER BY `name` ASC');
 			$result = $query->execute(array($fileId));
 			if (\OC_DB::isError($result)) {
-				\OCP\Util::writeLog('cache', 'getFolderContents failed: '.$result->getMessage(), \OCP\Util::ERROR);
+				\OCP\Util::writeLog('cache', 'getFolderContents failed: ' . $result->getMessage(), \OCP\Util::ERROR);
 			}
 			$files = $result->fetchAll();
 			foreach ($files as &$file) {
@@ -198,7 +198,7 @@ class Cache {
 				. ' VALUES(' . implode(', ', $valuesPlaceholder) . ')');
 			$result = $query->execute($params);
 			if (\OC_DB::isError($result)) {
-				\OCP\Util::writeLog('cache', 'Insert to cache failed: '.$result->getMessage(), \OCP\Util::ERROR);
+				\OCP\Util::writeLog('cache', 'Insert to cache failed: ' . $result->getMessage(), \OCP\Util::ERROR);
 			}
 
 			return (int)\OC_DB::insertid('*PREFIX*filecache');
@@ -362,7 +362,7 @@ class Cache {
 		$query = \OC_DB::prepare('SELECT `size` FROM `*PREFIX*filecache` WHERE `storage` = ? AND `path_hash` = ?');
 		$result = $query->execute(array($this->getNumericStorageId(), $pathHash));
 		if( \OC_DB::isError($result)) {
-			\OCP\Util::writeLog('cache', 'get status failed: '.$result->getMessage(), \OCP\Util::ERROR);
+			\OCP\Util::writeLog('cache', 'get status failed: ' . $result->getMessage(), \OCP\Util::ERROR);
 		}
 		if ($row = $result->fetchRow()) {
 			if ((int)$row['size'] === -1) {
@@ -504,7 +504,7 @@ class Cache {
 			. ' WHERE `storage` = ? AND `size` = -1 ORDER BY `fileid` DESC',1);
 		$result = $query->execute(array($this->getNumericStorageId()));
 		if (\OC_DB::isError($result)) {
-			\OCP\Util::writeLog('cache', 'getIncomplete failed: '.$result->getMessage(), \OCP\Util::ERROR);
+			\OCP\Util::writeLog('cache', 'getIncomplete failed: ' . $result->getMessage(), \OCP\Util::ERROR);
 		}
 		if ($row = $result->fetchRow()) {
 			return $row['path'];
